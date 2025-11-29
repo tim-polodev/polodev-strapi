1a3c7d7483562b3dad98b7374626eea7dd90775d
Polodev <polodev.com@gmail.com>
2025-11-29 10:06:45 +0700

    Add CORS configuration middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -31,9 +31,23 @@ export default [
             },
         },
     },
-    'strapi::cors',
+    {
+        name: 'strapi::cors',
+        config: {
+            origin: process.env.ALLOWED_CORS_DOMAINS?.split(',') || [],
+            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+            headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+            keepHeaderOnError: true,
+        },
+    },
     'strapi::poweredBy',
-    'strapi::query',
+    {
+        name: 'strapi::query',
+        config: {
+            arrayLimit: 50,
+            depth: 10,
+        },
+    },
     'strapi::body',
     'strapi::session',
     'strapi::favicon',
